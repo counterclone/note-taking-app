157427a75a4cf97ea4377fbaa56ff3c877bef6c2
devansh <devansh2102003@gmail.com>
2025-04-23 01:00:39 +0530

    tea

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,8 +25,8 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="w-full max-w-md rounded-lg p-8 shadow-md">
+    <div className="flex min-h-screen items-center bg-purple justify-center">
+      <div className="w-full max-w-md rounded-lg  p-8 shadow-md">
         <h1 className="mb-6 text-center text-2xl font-bold">Login</h1>
 
         {error && <div className="mb-4 text-red-500">{error}</div>}
